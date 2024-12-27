9ce9990deb3b3b83b595928ec8b5a41113d18714
S. M. SADI <smsadi2001@gmail.com>
2024-12-27 18:06:29 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const password = document.querySelector('#password');
 const togglePassword = document.querySelector('#togglePassword');
+const submit = document.querySelector('#submit');
 
 togglePassword.addEventListener("click",()=>{
     const type = password.getAttribute("type") === "password" ? "text" : "password" ;
     password.setAttribute("type",type);
 
     togglePassword.classList.toggle("bi-eye");
+})
+
+submit.addEventListener("click",()=>{
+    alert("Form Submitted, Thank You")
 })
